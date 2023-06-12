8b0832db1940173b21581f96bad262118bdae262
Vladislav <lycakvladislav@gmail.com>
2023-06-12 16:21:19 -0400

    fix firebase routing

--- src/components/LoadingPage/LoadingPage.jsx
+++ src/components/LoadingPage/LoadingPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './LoadingPage.module.css';
-import PulseLoader from 'react-spinners/PulseLoader';
+import FadeLoader from 'react-spinners/FadeLoader';
 
 const LoadingPage = () => {
   return (
     <div className={styles.main}>
       <div className={styles.block}>
-        <PulseLoader color="#36d7b7" size={100} />
+        <FadeLoader color="#2e8b43" size={30} />
         <p> Loading...</p>
       </div>
     </div>
