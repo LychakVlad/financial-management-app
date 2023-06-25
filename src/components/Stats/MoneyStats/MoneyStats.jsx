4e303154144593699d533fce5381e0e3eba4cd79
Vladislav <lycakvladislav@gmail.com>
2023-06-24 20:25:29 -0400

    add modal component

--- src/components/Stats/MoneyStats/MoneyStats.jsx
+++ src/components/Stats/MoneyStats/MoneyStats.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './MoneyStats.module.css';
 import { formatNumber } from '../../../utils/formatNumber';
 import { useSelector } from 'react-redux';
+import CustomButton from '../../form/Button/CustomButton';
+import Modal from '../../ui/Modal/Modal';
 
 const MoneyStats = () => {
+  const [openModal, setOpenModal] = useState(false);
   const { expenses } = useSelector((state) => state.expenses);
   const { totalCard, totalCash } = useSelector((state) => state.incomes);
 
   const totalSavings = expenses
     .filter((item) => item.type === 'Savings')
     .reduce((total, item) => total + parseFloat(item.amount), 0);
+
+  const handleOpenModal = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.info}>
@@ -18,7 +26,10 @@ const MoneyStats = () => {
         <p>Money in cash: {formatNumber(totalCash) + ' $'}</p>
         <p>Money on Card: {formatNumber(totalCard) + ' $'}</p>
       </div>{' '}
-      <div className={styles.buttons}></div>
+      <div className={styles.button}>
+        <CustomButton title={'Transfer money'} onClick={handleOpenModal} />
+      </div>
+      {openModal && <Modal handleClose={handleOpenModal}>Hello</Modal>}
     </div>
   );
 };
---
+++ src/components/ui/Modal/Modal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Modal.module.css';
+import { ReactComponent as Close } from '../../../assets/close-icon.svg';
+
+const Modal = ({ children, handleClose }) => {
+  const handleClick = () => {
+    handleClose(false);
+  };
+  return (
+    <div className={styles.modal}>
+      <div className={styles.wrapper}>
+        <button className="modal__close-icon" onClick={handleClick}>
+          <Close />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
