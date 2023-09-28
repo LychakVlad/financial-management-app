38e334e6ddf46a4511bd7b76cce058d94a33846c
Vladislav <lycakvladislav@gmail.com>
2023-09-27 20:02:12 -0400

    fix cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  baseUrl: 'http://localhost:3000',
+  integrationFolder: 'src/cypress/e2e',
+  testFiles: '**/*.cy.js',
   e2e: {
     setupNodeEvents(on, config) {},
   },
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-const app = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-});
+};
 
-export const auth = app.auth();
-export const firestore = app.firestore();
+const app = initializeApp(firebaseConfig);
 
-export default app;
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
